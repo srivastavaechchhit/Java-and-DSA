4c97cb3aec7854460a0fc2d4fa4ef7ecf9320ca7
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-05 20:20:22 +0530

    Add reading a character

---
+++ 3. Taking Input in Java/Codes/ReadChar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReadChar {
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a character:");
+        char ch = sc.next().charAt(0);  // Extracting the character present at 0th index of the String entered by the user
+
+        System.out.println("Character is: " +ch);
+    }
+}
