504e3b2a2690cfed601e87132b49b01d9ec22aa9
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-05 18:43:30 +0530

    Add DataTypes program

---
+++ 2. Variables and Data Types/Codes/DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+  
+    public static void main(String[] args) {
+        boolean flag = true;
+        int a = 12345;
+        short s = 1200;
+        byte b = 120;
+        long l = 832786310;
+        float f = 800.219735f;
+        double d = 7878.8765;
+        char ch = 'M';
+
+        System.out.println("The boolean variable is: " +flag);
+        System.out.println("The int variable is: " +a);
+        System.out.println("The short variable is: " +s);
+        System.out.println("The byte variable is: " +b);
+        System.out.println("The long variable is: " +l);
+        System.out.println("The float variable is: " +f);
+        System.out.println("The double variable is: " +d);
+        System.out.println("The char variable is: " +ch);
+    }
+}
