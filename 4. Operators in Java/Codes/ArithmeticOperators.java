e5dfc97e956715ddaec9c542019ec094682ce6ec
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-06 20:59:02 +0530

    Add Arithmetic Operators

---
+++ 4. Operators in Java/Codes/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+  
+    public static void main(String[] args) {
+        int p = 21, q = 10;
+
+        System.out.println(p+q);
+        System.out.println(p-q);
+        System.out.println(p*q);
+        System.out.println(p/q);
+        System.out.println(p%q);
+    }
+}
