880dbdbb0f87c72fa498213d15b4710564ff831c
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-10 11:45:05 +0530

    Update by adding for loop

--- 6. Loops in Java/Codes/Sum.java
+++ 6. Loops in Java/Codes/Sum.java
@@ -1,3 +1,8 @@
+/* This program contains two approaches to find the sum of first n natural numbers -
+   Using while loop and using for loop
+   Use only one block of code at once in order to avoid errors
+*/
+
 import java.util.Scanner;
 
 public class SumOfNumbers {
@@ -10,9 +15,16 @@ public class SumOfNumbers {
         System.out.println("Enter n:");
         int num = sc.nextInt();
 
+      // Using while loop
         while (count <= num) {
             sum += count;
             count++;
+        }
+        System.out.println("Sum of " +num+ " numbers is: " +sum);
+
+      // Using for loop
+      for (int count = 1; count <=num; count++) {
+            sum += count;
         }
         System.out.println("Sum of " +num+ " numbers is: " +sum);
     }
