9400527f055ed3abb4abf99318ccd7766cb9680c
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-10 13:54:45 +0530

    Program to find sun of a given series

---
+++ 6. Loops in Java/Codes/SeriesSum.java
@@ -0,0 +1,25 @@
+// S = 1 - 2 + 3 - 4 + 5 - 6...n
+
+import java.util.Scanner;
+
+public class SumSeries {
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i=1; i<=n; i++) {
+            if (i % 2 == 0) {
+                sum -= i;
+            }
+            else {
+                sum += i;
+            }
+        }
+        System.out.println("Sum is " +sum);
+    }
+}
