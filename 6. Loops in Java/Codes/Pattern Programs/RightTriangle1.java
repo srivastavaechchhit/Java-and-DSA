811c5deb4b6a432732aff3354f323e6b4322aa0f
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-10 15:34:49 +0530

    Add RightTriangle Program

---
+++ 6. Loops in Java/Codes/Pattern Programs/RightTriangle1.java
@@ -0,0 +1,26 @@
+/*
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+ */
+
+import java.util.Scanner;
+
+public class RightTriangle1 {
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no. of rows:");
+        int row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
