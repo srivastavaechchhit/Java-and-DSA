b0147e3f9177249c3929dd5be95b5e0e68ebcad7
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-12 13:17:11 +0530

    Accessing a member variable through an object

---
+++ 8. Introduction to OOPs/Codes/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    int yearOfManufacture;
+    String colour;
+
+    public static void main(String[] args) {
+        Car obj1 = new Car();
+
+        System.out.println(obj1.yearOfManufacture);
+        System.out.println(obj1.colour);
+    }
+}
