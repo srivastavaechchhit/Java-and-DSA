820c84eeba1515c8400094fea53ceff4e040b704
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-12 13:59:28 +0530

    In-built methods

---
+++ 8. Introduction to OOPs/Java Methods/Codes/StandardLibraryMethods.java
@@ -0,0 +1,14 @@
+public class StandardLibraryMethods {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.sqrt(24));
+        System.out.println(Math.sqrt(16));
+
+        System.out.println(Math.floor(5.3));
+        System.out.println(Math.floor(5.8));
+
+        System.out.println(Math.ceil(5.3));
+        System.out.println(Math.ceil(5.8));
+    }
+}
