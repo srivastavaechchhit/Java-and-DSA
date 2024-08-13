f406bbaeb36f4056a4ef2e100b6a1819631db5bd
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-13 22:45:05 +0530

    Program to find index of 'x'

---
+++ 9. Arrays/1. Single-dimensional Arrays/Codes/Index.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Index {
+
+    static int firstIndex(int[] arr, int x) {
+        int index = -1;
+        for (int nums : arr) {
+            if (nums == x)
+                return index;
+        }
+        return index;
+    }
+
+    static int lastIndex(int[] arr, int x) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x)
+                index = i;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Array size:");
+        int size = sc.nextInt();
+
+        int[] arr = new int [size];
+
+        System.out.println("Enter " +size+ " elements in the array:");
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.println("Enter x:");
+        int x = sc.nextInt();
+
+        System.out.println("First index of x: " + firstIndex(arr, x));
+        System.out.println("Last index of x: " +lastIndex(arr, x));
+    }
+}
