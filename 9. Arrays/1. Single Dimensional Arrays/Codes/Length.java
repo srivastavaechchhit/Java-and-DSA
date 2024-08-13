8f1f0bde5bc0e4ee4c2dd23dd13dc41c8ae96c42
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-13 17:18:22 +0530

    Add Length. program

---
+++ 9. Arrays/1. Single Dimensional Arrays/Codes/Length.java
@@ -0,0 +1,11 @@
+public class Length {
+
+    static int findArrayLength(int[] arr) {
+        return arr.length;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {5, 6, 7, 8, 9};
+        System.out.println("Length of the given Array is: " +findArrayLength(arr));
+    }
+}
