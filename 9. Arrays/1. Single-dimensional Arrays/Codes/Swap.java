b4af3cc50bb55695a69e824b1c62bd81b151649e
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-14 15:34:49 +0530

    Add Swap program
    Given two integers 'a' and 'b'. Swap the two given values using temporary variables.

---
+++ 9. Arrays/1. Single-dimensional Arrays/Codes/Swap.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Swap {
+
+    static void usingTemp(int a, int b) {
+        System.out.println("Values before swapping:");
+        System.out.println("a: " +a);
+        System.out.println("b: " +b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Values after swapping:");
+        System.out.println("a: " +a);
+        System.out.println("b: " +b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter b:");
+        int b = sc.nextInt();
+
+        usingTemp(a, b);
+    }
+}
