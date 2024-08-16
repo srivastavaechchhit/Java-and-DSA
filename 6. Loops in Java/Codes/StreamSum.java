c97492b554f548bc23cca53774a8df5be77c6503
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-16 10:18:54 +0530

    Fixed code

--- 6. Loops in Java/Codes/StreamSum.java
+++ 6. Loops in Java/Codes/StreamSum.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class StreamSum {
   
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int sum = 0, num;
-        System.out.println("Enter positive Integers:");
-
-        do {
-            num = sc.nextInt();
-            sum += num;
-        } while (num > 0);
-        System.out.println("Sum: " +sum);
-    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int sum = 0, num;
+    System.out.println("Enter positive Integers:");
+    int num = sc.nextInt();
+    
+    do {
+      num = sc.nextInt();
+      sum += num;
+    } while (num != -1);
+    System.out.println("Sum: " +sum);
+  }
 }
