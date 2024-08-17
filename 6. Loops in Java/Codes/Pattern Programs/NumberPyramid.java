fdb09b001db845faa92ebb9c9a2c2b6f78a58b63
Echchhit Srivastava <srivastavaechchhit.2021@gmail.com>
2024-08-17 17:41:59 +0530

    Update NumberPyramid

--- 6. Loops in Java/Codes/Pattern Programs/NumberPyramid.java
+++ 6. Loops in Java/Codes/Pattern Programs/NumberPyramid.java
@@ -8,7 +8,7 @@
 
 import java.util.Scanner;
 
-public class Palindrome {
+public class NumberPyramid {
   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
